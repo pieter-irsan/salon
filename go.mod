34328a96604308bb438d829f20190994ace52631
pieter-irsan <pieterirsan.pi@gmail.com>
2026-02-22 23:32:05 +0700

    feat: add config package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module salon
 
 go 1.26.0
+
+require github.com/caarlos0/env/v11 v11.3.1
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+	"log"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type Config struct {
+	Port     string `env:"PORT" envDefault:"2001"`
+	SalonDsn string `env:"SALON_DSN,notEmpty"`
+}
+
+func New() Config {
+	c, err := env.ParseAs[Config]()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c
+}
