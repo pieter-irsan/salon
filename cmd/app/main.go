a1cbba715a35d00dbe6b1a00fc9c52ba64a865ac
Pieter Irsan Hutagaol <pieterirsan.pi@gmail.com>
2026-02-22 19:44:18 +0700

    chore: init project structure

---
+++ cmd/app/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	// init config
+
+	// init logger
+
+	// init db
+
+	// init repo
+
+	// init service
+
+	// init handler
+
+	// init router
+
+	// init cancellation ctx
+
+	// run router
+}
