f9777e48aa90022d3b25623f961830a72868fc1b
pieter-irsan <pieterirsan.pi@gmail.com>
2026-02-22 23:59:00 +0700

    feat: add logger package

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,25 @@
 package logger
+
+import (
+	"log/slog"
+	"os"
+	"time"
+)
+
+func New() *slog.Logger {
+	return slog.New(
+		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource:   true,
+			Level:       slog.LevelDebug,
+			ReplaceAttr: formatTimestamp,
+		}),
+	)
+}
+
+func formatTimestamp(groups []string, attr slog.Attr) slog.Attr {
+	if attr.Key == slog.TimeKey {
+		t := attr.Value.Time().Format(time.DateTime)
+		attr.Value = slog.StringValue(t)
+	}
+	return attr
+}
