b85db6e3aef7bfdad245fc73983a77acc79305e1
pieter-irsan <pieterirsan.pi@gmail.com>
2026-02-23 00:50:04 +0700

    feat: add db package

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,26 @@
 package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func New(dsn string) *pgxpool.Pool {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return pool
+}
